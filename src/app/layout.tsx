d8080fda630be18f4e257a6d819d21c75bb7f2d8
emoking8848 <wy1997ycq@gmail.com>
2026-04-28 14:44:04 +0800

    Add Google Ads tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,14 @@ import { CookieConsentBanner } from "@/components/cookie-consent-banner";
 
 import "./globals.css";
 
+const googleAdsTagScript = `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-18075468770');
+`;
+
 export const metadata: Metadata = {
   title: "Joheiewisepro | Always Competitively Priced",
   description: "Joheiewisepro | Always Competitively Priced",
@@ -16,6 +24,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full antialiased">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-18075468770" />
+        <script dangerouslySetInnerHTML={{ __html: googleAdsTagScript }} />
+      </head>
       <body className="min-h-full flex flex-col">
         {children}
         <CookieConsentBanner />
