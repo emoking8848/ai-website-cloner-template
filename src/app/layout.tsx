5eb2fb54790e2c5b92764e7710f68311d4bb591f
emoking8848 <wy1997ycq@gmail.com>
2026-04-29 03:51:43 +0800

    google 代码版本

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,57 +4,14 @@ import { CookieConsentBanner } from "@/components/cookie-consent-banner";
 
 import "./globals.css";
 
-const googleAdsTagScript = `
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+const GTM_ID = "GTM-NT3J3G2H";
 
-  gtag('config', 'AW-18075628392');
-`;
-
-const googleAdsPurchaseConversionScript = `
-  (function () {
-    function readOrderValue() {
-      var totalNode = document.querySelector(
-        '.woocommerce-order-overview__total .amount, .order_details .total .amount, .woocommerce-table--order-details tfoot .amount'
-      );
-      if (!totalNode) {
-        return { value: 1.0, currency: 'USD' };
-      }
-
-      var text = totalNode.textContent || '';
-      var normalized = text.replace(/[^0-9.,]/g, '').replace(/,/g, '');
-      var value = parseFloat(normalized);
-      var currency = text.indexOf('£') !== -1 ? 'GBP' : text.indexOf('€') !== -1 ? 'EUR' : 'USD';
-
-      return {
-        value: Number.isFinite(value) && value > 0 ? value : 1.0,
-        currency: currency
-      };
-    }
-
-    function sendPurchaseConversion() {
-      if (typeof gtag !== 'function' || !/\\/checkout\\/order-received\\//.test(window.location.pathname)) {
-        return;
-      }
-
-      var match = window.location.pathname.match(/\\/order-received\\/([^/]+)/);
-      var order = readOrderValue();
-
-      gtag('event', 'conversion', {
-        'send_to': 'AW-18075628392/8Y3HCOrBs58cEOjmkKtD',
-        'value': order.value,
-        'currency': order.currency,
-        'transaction_id': match ? match[1] : ''
-      });
-    }
-
-    if (document.readyState === 'loading') {
-      document.addEventListener('DOMContentLoaded', sendPurchaseConversion, { once: true });
-    } else {
-      sendPurchaseConversion();
-    }
-  }());
+const googleTagManagerScript = `
+  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','${GTM_ID}');
 `;
 
 export const metadata: Metadata = {
@@ -70,11 +27,18 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full antialiased">
       <head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-18075628392" />
-        <script dangerouslySetInnerHTML={{ __html: googleAdsTagScript }} />
-        <script dangerouslySetInnerHTML={{ __html: googleAdsPurchaseConversionScript }} />
+        <script dangerouslySetInnerHTML={{ __html: googleTagManagerScript }} />
       </head>
       <body className="min-h-full flex flex-col">
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            title="Google Tag Manager"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         {children}
         <CookieConsentBanner />
       </body>
